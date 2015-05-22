a15fe93fef1af944c0b16a7b61279378bab0bfb9
NGO THANH TUNG SOFTECH <TUNGNT@NGOs-iMac.local>
2015-05-22 22:48:56 +0700

    Initial Commit

---
+++ Education/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Education
+//
+//  Created by NGO THANH TUNG SOFTECH on 5/22/15.
+//  Copyright (c) 2015 Softech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
